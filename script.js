0a9df58f1434bc32c1d234a62d37c4a0aff8087d
jaujau088 <jaujau088@gmail.com>
2022-04-18 15:11:28 +0200

    Add local storage feature

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Form local storage availability checker function
+function isStorageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      // 1) everything except Firefox
+      // 2) Firefox
+      // test name field too, because code might not be present
+      // 3) everything except Firefox
+      // 4) Firefox
+      // 5) acknowledge QuotaExceededError only if there's something already stored
+      e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
+  }
+}
+
+//
 const title = document.querySelector('#book-title');
 const author = document.querySelector('#book-author');
 const form = document.querySelector('#form-add-book');
@@ -5,13 +27,21 @@ const bookList = document.querySelector('.book-list');
 
 let books = [];
 
-form.onsubmit = (e) => {
+const setData = () => {
   let book = {
     'title': title.value,
     'author': author.value,
   };
 
-  e.preventDefault();
+  books.push(book);
 
-  console.log(book);
+  if (isStorageAvailable('localStorage')) {
+    localStorage.setItem('bookList', JSON.stringify(books));
+  }
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  
+  setData();
 };
